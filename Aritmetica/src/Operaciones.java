0a0f32898761d9df44a59a2182de7ab532ee4e06
alsebher <114057137+alsebher@users.noreply.github.com>
2022-09-29 13:07:28 +0200

    Add ProgramaAritmetica

---
+++ Aritmetica/src/Operaciones.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class Operaciones {
+    public int sumar(int a,int b){
+        return a+b;
+    }
+    
+    public int restar(int a,int b){
+        return a-b;
+    }
+    
+    public int multiplicar(int a,int b){
+        return a*b;
+    }
+    
+    public double dividir(int a,int b){
+        return a/b;
+    }
+    
+}
